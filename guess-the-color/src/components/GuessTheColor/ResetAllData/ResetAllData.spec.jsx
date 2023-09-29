b293bd8ac2ad821c28e14c09589d0f15fbcc341c
Danilo Dias <danilolive30@hotmail.com>
2023-09-29 16:22:21 -0300

    feat: button reset all data teste

---
+++ guess-the-color/src/components/GuessTheColor/ResetAllData/ResetAllData.spec.jsx
@@ -0,0 +1,26 @@
+import { describe, it } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ResetAllData } from ".";
+import { GuessTheColorProvider } from "../../../contexts/GuessTheColorContext";
+import { TimeRemaningProvider } from "../../../contexts/TimeRemaningContext";
+import { CurrentLatestGameProvider } from "../../../contexts/CurrentLatestGameContext";
+
+describe("Render ResetAllData", () => {
+  it("Render ResetAllData", () => {
+    render(
+      <TimeRemaningProvider>
+        <CurrentLatestGameProvider>
+          <GuessTheColorProvider
+            initialValue={{
+              start: false,
+            }}
+          >
+            <ResetAllData />
+          </GuessTheColorProvider>
+        </CurrentLatestGameProvider>
+      </TimeRemaningProvider>
+    );
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+  });
+});

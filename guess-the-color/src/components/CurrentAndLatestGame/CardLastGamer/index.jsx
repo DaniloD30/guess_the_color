8eb0754c471bfbf5832fea0d5ce57fde2b4e4de7
Danilo Dias <danilolive30@hotmail.com>
2023-09-29 01:13:04 -0300

    feat: get contrast color

--- guess-the-color/src/components/CurrentAndLatestGame/CardLastGamer/index.jsx
+++ guess-the-color/src/components/CurrentAndLatestGame/CardLastGamer/index.jsx
@@ -5,6 +5,16 @@ export function CardLastGamer() {
   const value = useCurrentLatestGameContext();
   const reverseArrGame = [...value.arrGames.items].reverse();
 
+  function getContrastColor(backgroundColor) {
+    if (backgroundColor) {
+      const r = parseInt(backgroundColor.slice(1, 3), 16);
+      const g = parseInt(backgroundColor.slice(3, 5), 16);
+      const b = parseInt(backgroundColor.slice(5, 7), 16);
+      const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+      return brightness > 128 ? "#000000" : "#ffffff";
+    }
+    return "#ffffff";
+  }
   return (
     <>
       {reverseArrGame.map((game, index) =>
@@ -17,6 +27,8 @@ export function CardLastGamer() {
               className="correct-box"
               style={{
                 backgroundColor: `${game.colorCorrect}`,
+                color: `${getContrastColor(game.colorCorrect)}`,
+                fontWeight: 600
               }}
             >
               {game.colorCorrect}
@@ -37,6 +49,8 @@ export function CardLastGamer() {
               className="invalid-box"
               style={{
                 backgroundColor: `${game.guessedColor}`,
+                color: `${getContrastColor(game.guessedColor)}`,
+                fontWeight: 600
               }}
             >
               {game.guessedColor}
@@ -45,6 +59,8 @@ export function CardLastGamer() {
               className="invalid-box"
               style={{
                 backgroundColor: `${game.colorCorrect}`,
+                color: `${getContrastColor(game.guessedColor)}`,
+                fontWeight: 600
               }}
             >
               {game.colorCorrect}
